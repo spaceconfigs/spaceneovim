3dfc105857c25b9ee0fd2ad03174d8c67f9f7a24
Nitai Charan <nitaicharan@gmail.com>
2024-04-19 16:15:11 +0100

    fix(statusline): remove blank statusline

--- lua/infraestrucuture/plugins/org.lua
+++ lua/infraestrucuture/plugins/org.lua
@@ -3,14 +3,6 @@ if not neogit_ok then
 	return vim.notify("Failed to load plugin `nvim-orgmode/orgmode`")
 end
 
-plugin.setup({
-	org_agenda_files = "~/Notes/**/*",
-	org_default_notes_file = "~/Notes/init.org",
-  mappings = {
-		disable_all = true,
-	},
-})
-
-plugin.setup_ts_grammar()
+plugin.setup()
 
 return plugin
