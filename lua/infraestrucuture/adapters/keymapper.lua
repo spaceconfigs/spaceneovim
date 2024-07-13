c232931fbbab88f4dff2a9b9856fa4e831d356bc
Nitai Charan <nitaicharan@gmail.com>
2024-07-13 12:10:39 +0100

    feat(keymappter): apply required changes for new version off which-key

--- lua/infraestrucuture/adapters/keymapper.lua
+++ lua/infraestrucuture/adapters/keymapper.lua
@@ -4,70 +4,63 @@ local logger_use_manage = require("application.use_cases.logger")
 local plugin = require("infraestrucuture.plugins.keymapper")
 
 local format_map = function(map)
-  return {
-    [map.key] = {
-      map.description,
-      map.method,
-      buffer = map.buffer,
-      silent = map.silent,
-      noremap = map.noremap,
-      nowait = map.nowait,
-      mode = map.mode,
-    },
-  }
+	return {
+		map.key,
+		desc = map.description,
+		map.method,
+		buffer = map.buffer,
+		silent = map.silent,
+		noremap = map.noremap,
+		nowait = map.nowait,
+		mode = map.mode,
+	}
 end
 
 local format_group = function(map)
-  return {
-    [map.key] = {
-      name = map.description,
-      buffer = map.buffer,
-      silent = map.silent,
-      noremap = map.noremap,
-      nowait = map.nowait,
-      mode = map.mode,
-    },
-  }
+	return {
+		map.key,
+		group = map.description,
+	}
 end
 
 M.format = function(map)
-  local message = {
-    module = "adapters/keymapper",
-    func = "format",
-    server = map,
-  }
-  logger_use_manage.debug(message)
+	local message = {
+		module = "adapters/keymapper",
+		func = "format",
+		server = map,
+	}
+	logger_use_manage.debug(message)
 
-  if map.method == nil then
-    return format_group(map)
-  end
+	if map.method == nil then
+		return format_group(map)
+	end
 
-  return format_map(map)
+	return format_map(map)
 end
 
 M.register = function(map)
-  local message = {
-    module = "adapters/keymapper",
-    func = "register",
-    server = map,
-  }
-  logger_use_manage.debug(message)
+	local message = {
+		module = "adapters/keymapper",
+		func = "register",
+		server = map,
+	}
+	logger_use_manage.debug(message)
 
-  local formatted = M.format(map)
-  plugin.register(formatted)
+	local formatted = M.format(map)
+	plugin.add(formatted)
 end
 
 M.register_all = function(maps)
-  local message = {
-    module = "adapters/keymapper",
-    func = "regist_all",
-    server = maps,
-  }
-  logger_use_manage.debug(message)
+	local message = {
+		module = "adapters/keymapper",
+		func = "regist_all",
+		server = maps,
+	}
+	logger_use_manage.debug(message)
 
-  for _, map in pairs(maps) do
-    M.register(map)
-  end
+	for _, map in pairs(maps) do
+		M.register(map)
+	end
 end
 
 -- local maps = require("domain.maps")
--- lua/infraestrucuture/plugins/lazy/configs/finder.lua
+++ lua/infraestrucuture/plugins/lazy/configs/finder.lua
@@ -1,12 +1,13 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-ui-select.nvim",
-		"nvim-telescope/telescope-file-browser.nvim",
-		"nvim-telescope/telescope-frecency.nvim",
-		"kkharji/sqlite.lua",
-		"nvim-tree/nvim-web-devicons",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+			"nvim-telescope/telescope-frecency.nvim",
+			"kkharji/sqlite.lua",
+			"nvim-tree/nvim-web-devicons",
+		},
 	},
 }
