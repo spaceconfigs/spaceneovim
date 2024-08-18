3b480dfc96ec0ba240b2fa550322e016ffacab37
Nitai Charan <nitaicharan@gmail.com>
2024-08-18 17:18:05 +0100

    feat(hooks): setup notification on start up

--- lua/application/hooks/lifecycle.lua
+++ lua/application/hooks/lifecycle.lua
@@ -5,6 +5,7 @@ local theme_use_case = require("application.use_cases.theme")
 local completer_use_case = require("application.use_cases.completer")
 local keymaper_usecase = require("application.use_cases.keymapper")
 local logger_use_manage = require("application.use_cases.logger")
+local notification_use_manage = require("application.use_cases.notification")
 
 M.onModuleInit = function()
 	local message = {
@@ -14,6 +15,7 @@ M.onModuleInit = function()
 	logger_use_manage.debug(message)
 	theme_use_case.load()
 	completer_use_case.setup()
+	notification_use_manage.setup()
 
 	keymaper_usecase.register_all(maps)
 end
