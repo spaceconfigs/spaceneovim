54c0ef4a67d1b2a8762b5b95da92ed2ef56dc0ad
Nitai Charan <nitaicharan@gmail.com>
2024-08-18 17:26:22 +0100

    feat(maps): remove add layout

--- lua/domain/maps/project.lua
+++ lua/domain/maps/project.lua
@@ -1,5 +1,4 @@
 local terminal_use_case = require("application.use_cases.terminal")
-local layout_use_case = require("application.use_cases.layout")
 local workspace_use_case = require("application.use_cases.workspace")
 local tree_use_case = require("application.use_cases.tree")
 
@@ -7,7 +6,6 @@ local project_content = {
 	{ key = "'", description = "Terminal", method = terminal_use_case.toggle },
 	{ key = "E", description = "Diagnostics", method = workspace_use_case.diagnostics },
 	{ key = "f", description = "Find File", method = workspace_use_case.files },
-	{ key = "l", description = "Add to layout", method = layout_use_case.save },
 	{ key = "p", description = "Find Projects", method = workspace_use_case.open },
 	{
 		key = "t",
