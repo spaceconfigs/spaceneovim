68d0187f1d1b241834b4a7044044736809ce6547
Nitai Charan <nitaicharan@gmail.com>
2024-08-22 20:00:55 +0100

    feat(test): add module to test

---
+++ lua/infraestrucuture/plugins/lazy/configs/tester.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-jest",
+			"rcasia/neotest-java",
+		},
+	},
+}
