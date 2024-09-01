c86196b7578ce030a0aaf15754e8ebc7cdbf6dba
Nitai Charan <nitaicharan@gmail.com>
2024-09-01 16:28:47 +0100

    fix(tree): change bind to correct default

--- lua/infraestrucuture/plugins/tree.lua
+++ lua/infraestrucuture/plugins/tree.lua
@@ -21,28 +21,28 @@ local on_attach = function(bufnr)
     }
   end
 
+  vim.keymap.set("n", "?", nvim_tree_api.tree.toggle_help, opts("Help"))
+  vim.keymap.set("n", "<CR>", nvim_tree_api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "cf", nvim_tree_api.fs.create, opts("Create"))
   vim.keymap.set("n", "d", nvim_tree_api.fs.remove, opts("Delete"))
+  vim.keymap.set("n", "gr", nvim_tree_api.tree.reload, opts("Refresh"))
   vim.keymap.set("n", "h", nvim_tree_api.node.navigate.parent, opts("Parent Directory"))
   vim.keymap.set("n", "H", nvim_tree_api.node.navigate.parent_close, opts("Close Directory"))
-  vim.keymap.set("n", "R", nvim_tree_api.fs.rename, opts("Rename"))
+  vim.keymap.set("n", "l", nvim_tree_api.node.open.edit, opts("Open"))
   vim.keymap.set("n", "m", nvim_tree_api.marks.bulk.move, opts("Move Bookmarked"))
-  vim.keymap.set("n", "p", nvim_tree_api.fs.paste, opts("Paste"))
-  vim.keymap.set("n", "P", nvim_tree_api.node.open.preview, opts("Open Preview"))
-  vim.keymap.set("n", "cf", nvim_tree_api.fs.create, opts("Create"))
-  vim.keymap.set("n", "gr", nvim_tree_api.tree.reload, opts("Refresh"))
   vim.keymap.set("n", "oh", nvim_tree_api.node.open.horizontal, opts("Open: Horizontal Split"))
+  vim.keymap.set("n", "oo", nvim_tree_api.node.open.edit, opts("Open"))
+  -- TODO: set 'oo' to tree and file on buffer
   vim.keymap.set("n", "ov", nvim_tree_api.node.open.vertical, opts("Open: Vertical Split"))
   vim.keymap.set("n", "ox", nvim_tree_api.node.run.system, opts("Run System"))
-  vim.keymap.set("n", "yf", nvim_tree_api.fs.copy.node, opts("Copy"))
+  vim.keymap.set("n", "p", nvim_tree_api.fs.paste, opts("Paste"))
+  vim.keymap.set("n", "P", nvim_tree_api.node.open.preview, opts("Open Preview"))
+  vim.keymap.set("n", "R", nvim_tree_api.fs.rename, opts("Rename"))
+  vim.keymap.set("n", "th", nvim_tree_api.tree.toggle_hidden_filter, opts("Show dotfiles"))
   vim.keymap.set("n", "ya", nvim_tree_api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+  vim.keymap.set("n", "yf", nvim_tree_api.fs.copy.node, opts("Copy"))
   vim.keymap.set("n", "yp", nvim_tree_api.fs.copy.filename, opts("Copy Name"))
   vim.keymap.set("n", "yr", nvim_tree_api.fs.copy.relative_path, opts("Copy Relative Path"))
-  -- TODO: set 'oo' to tree and file on buffer
-  -- TODO: set 'CR' to open tree branch
-  vim.keymap.set("n", "oo", nvim_tree_api.node.open.edit, opts("Open"))
-  vim.keymap.set("n", "th", nvim_tree_api.tree.toggle_hidden_filter, opts("Show dotfiles"))
-  vim.keymap.set("n", "l", nvim_tree_api.node.open.edit, opts("Open"))
-  vim.keymap.set("n", "?", nvim_tree_api.tree.toggle_help, opts("Help"))
 end
 
 plugin.setup({
