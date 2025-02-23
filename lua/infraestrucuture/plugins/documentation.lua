86429485ee2a7f3adea7a22539a82c9f0900e1dc
Nitai Charan <nitaicharan@gmail.com>
2025-02-23 09:20:41 +0000

    feat(documentation): change from `DevdocsFloat` to `DevdocsOpen`

--- lua/infraestrucuture/plugins/documentation.lua
+++ lua/infraestrucuture/plugins/documentation.lua
@@ -1,10 +1,6 @@
 local ok, plugin = pcall(require, "nvim-devdocs")
 if not ok then
-	return vim.notify("Failed to load plugin `luckasRanarison/nvim-devdoc`")
+  return vim.notify("Failed to load plugin `nitaicharan/nvim-devdoc`")
 end
-
-plugin.setup({
-	previewer_cmd = "glow",
-})
 
 return plugin
