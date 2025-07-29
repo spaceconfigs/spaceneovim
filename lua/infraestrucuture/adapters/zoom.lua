927bf5bf626b5c4eef0608a50cff76f935e8b23f
Nitai Charan <nitaicharan@gmail.com>
2025-07-29 22:46:31 +0100

    feat(zoom): add zoom module

---
+++ lua/infraestrucuture/adapters/zoom.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+local logger_use_manage = require("application.use_cases.logger")
+local plugin = require("infraestrucuture.plugins.zoom")
+local gui_font_resize = plugin.gui_font_resize
+
+M.change = function(opts)
+	local message = {
+		module = "adapters/zoom",
+		func = "change",
+		opts = opts,
+	}
+	logger_use_manage.debug(message)
+
+	if opts.delta > 0 then
+		return vim.cmd("GUIFontSizeUp 1")
+	end
+
+	if opts.delta < 0 then
+		return vim.cmd("GUIFontSizeDown 1")
+	end
+
+	vim.cmd("GUIFontSizeSet")
+end
+
+return M
