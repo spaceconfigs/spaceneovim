66ee02d27b5cdb6ffc872fab177d127a2b069296
Nitai Charan <nitaicharan@gmail.com>
2025-07-29 22:47:59 +0100

    feat(search): add search for directory

--- lua/domain/maps/search.lua
+++ lua/domain/maps/search.lua
@@ -2,48 +2,66 @@ local searcher_use_case = require("application.use_cases.searcher")
 local workspace_use_case = require("application.use_cases.workspace")
 
 local grep_input_content = {
-  { key = "G",  description = "Grep input on project" },
-  { key = "Gp", description = "",                     method = workspace_use_case.grep_input },
+	{ key = "G", description = "Grep input on project" },
+	{ key = "Gp", description = "", method = workspace_use_case.grep_input },
 }
 
 local search_content = {
-  { key = "l", description = "Resume",                    method = searcher_use_case.resume() },
-  { key = "p", description = "Search in workspace",     method = searcher_use_case.search({ location = 'project', in_live = true }) },
-  { key = "P", description = "Search project w/ input", method = searcher_use_case.search_hover('project') },
-  { key = "s", description = "Text",                    method = searcher_use_case.search({ in_live = true }) },
-  { key = "S", description = "Search w/ input",         method = searcher_use_case.search_hover() },
+	{
+		key = "d",
+		description = "Directory",
+		method = searcher_use_case.search({ location = "directory" }),
+	},
+	{
+		key = "D",
+		description = "Directory",
+		method = searcher_use_case.search_hover({ location = "directory" }),
+	},
+	{ key = "l", description = "Resume", method = searcher_use_case.resume() },
+	{
+		key = "p",
+		description = "Search in workspace",
+		method = searcher_use_case.search({ location = "project", in_live = true }),
+	},
+	{
+		key = "P",
+		description = "Search project w/ input",
+		method = searcher_use_case.search_hover({ location = "project" }),
+	},
+	{ key = "s", description = "Text", method = searcher_use_case.search({ in_live = true }) },
+	{ key = "S", description = "Search w/ input", method = searcher_use_case.search_hover() },
 }
 
 local result = {}
 table.insert(result, {
-  key = "<leader>s",
-  description = "Search",
-  mode = "n",
-  buffer = nil,
-  silent = true,
-  noremap = true,
-  nowait = false,
+	key = "<leader>s",
+	description = "Search",
+	mode = "n",
+	buffer = nil,
+	silent = true,
+	noremap = true,
+	nowait = false,
 })
 
 local contents = {}
 for _, content in pairs({
-  grep_input_content,
-  search_content,
+	grep_input_content,
+	search_content,
 }) do
-  for _, bind in pairs(content) do
-    table.insert(contents, bind)
-  end
+	for _, bind in pairs(content) do
+		table.insert(contents, bind)
+	end
 end
 
 for _, content in pairs(contents) do
-  local bind = vim.deepcopy(content)
-  bind.key = "<leader>s" .. bind.key
-  bind.mode = "n"
-  bind.buffer = nil
-  bind.silent = true
-  bind.noremap = true
-  bind.nowait = false
-  table.insert(result, bind)
+	local bind = vim.deepcopy(content)
+	bind.key = "<leader>s" .. bind.key
+	bind.mode = "n"
+	bind.buffer = nil
+	bind.silent = true
+	bind.noremap = true
+	bind.nowait = false
+	table.insert(result, bind)
 end
 
 return result
--- lua/infraestrucuture/adapters/searcher.lua
+++ lua/infraestrucuture/adapters/searcher.lua
@@ -4,42 +4,46 @@ local logger_use_manage = require("application.use_cases.logger")
 local plugin = require("infraestrucuture.plugins.searcher")
 local snacks = plugin.snacks
 
--- snacks.picker.lines()
--- print(vim.inspect(snacks.picker))
--- snacks.picker.grep({
---   pattern = 'local'
--- })
-
 M.search = function(opts)
-  local message = {
-    module = "adapters/searcher",
-    func   = "search",
-    opts   = opts,
-  }
-  logger_use_manage.debug(message)
+	local message = {
+		module = "adapters/searcher",
+		func = "search",
+		opts = opts,
+	}
+	logger_use_manage.debug(message)
 
-  if opts.location == "project" then
-    return snacks.picker.grep({
-      layout = { preset = "nitaicharan" },
-      live = opts.text == nil,
-      search = opts.text,
-    })
-  end
+	if opts.location == "project" then
+		return snacks.picker.grep({
+			layout = { preset = "nitaicharan" },
+			live = opts.text == nil,
+			search = opts.text,
+			cwd = opts.path,
+		})
+	end
 
-  snacks.picker.lines({
-    layout = { preset = "nitaicharan_lines" },
-    pattern = opts.text,
-  })
+	if opts.location == "directory" then
+		return snacks.picker.grep({
+			layout = { preset = "nitaicharan" },
+			live = opts.text == nil,
+			search = opts.text,
+			cwd = opts.path,
+		})
+	end
+
+	snacks.picker.lines({
+		layout = { preset = "nitaicharan_lines" },
+		pattern = opts.text,
+	})
 end
 
 M.resume = function(opts)
-  local message = {
-    module = "adapters/searcher",
-    func   = "resume",
-    opts   = opts,
-  }
-  logger_use_manage.debug(message)
+	local message = {
+		module = "adapters/searcher",
+		func = "resume",
+		opts = opts,
+	}
+	logger_use_manage.debug(message)
 
-  snacks.picker.resume()
+	snacks.picker.resume()
 end
 return M
